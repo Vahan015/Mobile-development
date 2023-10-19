5f1e1a2b5dc0134c9b69a587d644caf0efa75e49
Vahan <vahan_babayan@edu.aua.am>
2023-10-20 03:45:23 +0400

    initial commit

---
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -0,0 +1,23 @@
+fun main() {
+    fun main() {
+        // Declare and initialize an array
+        val myArray = arrayOf(1, 2, 3, 4, 5)
+
+        // Declare and initialize a list
+        val myList = listOf("apple", "banana", "cherry", "date", "elderberry")
+
+        // Print elements of the array
+        println("Array elements:")
+        for (element in myArray) {
+            println(element)
+        }
+
+        // Print elements of the list
+        println("\nList elements:")
+        for (element in myList) {
+            println(element)
+        }
+    }
+
+
+}
